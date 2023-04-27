b911153a2fe963d640cbe9e02bb69c60a927af04
Thomas Prouvot <prouvot.t@gmail.com>
2023-04-27 11:23:54 +0200

    echo file list

--- help-functions.sh
+++ help-functions.sh
@@ -8,7 +8,7 @@ function export_nb_fields_no_desc(){
 	grep -r -L "<description>" force-app/main/default/objects/*/fields/*__c.field-meta.xml > $fileName
 
 	echo "File:"
-	echo $fileName
+	echo  > $fileName
 	NB_FIELD_NO_DESC=$(cat $fileName | sed -n '$=')
 	echo $NB_FIELD_NO_DESC
 }
